a7f56117ba8193ede2c60096418d3b0353000431
Rajat <rwlrajat@gmail.com>
2023-10-21 12:45:58 +0530

    updated website

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ setInterval(() => {
     hourTime = date.getHours();
     secondsTime = date.getSeconds();
     minuetsTime = date.getMinutes();
-    mllitime=date.getMilliseconds()
- 
+    mllitime = date.getMilliseconds()
+
     hRoatation = 30 * hourTime + minuetsTime / 2
-    mRoatation = 6 * minuetsTime + 0.1*secondsTime
-   
-    sRoatation = 6 * secondsTime +0.006 * mllitime
+    mRoatation = 6 * minuetsTime + 0.1 * secondsTime
+
+    sRoatation = 6 * secondsTime + 0.006 * mllitime
     hour.style.transform = `rotate(${hRoatation}deg)`
     minute.style.transform = `rotate(${mRoatation}deg)`
     second.style.transform = `rotate(${sRoatation}deg)`
